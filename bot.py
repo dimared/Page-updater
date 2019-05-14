ce8fe09a1300981d41981ef7a4b8b060a0ee289c
dimared <dimared@inbox.lv>
2019-05-14 16:59:38 +0300

    v1.0

---
+++ bot.py
@@ -0,0 +1,39 @@
+import json
+import requests
+import time
+
+
+def send_mess(chat, text):  
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(URL + 'sendMessage', data=params)
+    return response
+
+
+# Be careful with encoding
+with open("initial.json", "r", encoding='utf-8') as initial:
+    data = json.load(initial)
+
+URL = "https://api.telegram.org/bot" + data["token"] + "/"
+CHAT_ID = data["admin_id"]
+
+LINK = data["link"]
+TEXT = "Обновление на сайте: " + LINK
+ANCHOR_LEFT = data["anchor_left"] 
+ANCHOR_RIGHT = data["anchor_right"] 
+target = "-"
+
+while True:
+    req = requests.get(LINK)
+    html = req.text
+
+    # positions
+    pos_left = html.find(ANCHOR_LEFT)
+    pos_right = html[pos_left:].find(ANCHOR_RIGHT)
+
+    old_target = target
+    target = html[pos_left:pos_left+pos_right]
+
+    if target != old_target and old_target != None:
+        send_mess(CHAT_ID, TEXT)
+
+    time.sleep(6000)
